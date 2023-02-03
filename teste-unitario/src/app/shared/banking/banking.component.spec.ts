895282c1005f6771abbd2fcede36c85d624b281d
Also Ailton <ailton_gulmanelli@hotmail.com>
2023-02-03 19:47:02 -0300

    complete test app

---
+++ teste-unitario/src/app/shared/banking/banking.component.spec.ts
@@ -0,0 +1,83 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BankingComponent } from './banking.component';
+
+describe('BankingComponent', () => {
+  let component: BankingComponent;
+  let fixture: ComponentFixture<BankingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BankingComponent ]
+    })
+    .compileComponents();
+
+  });
+  beforeEach(() =>{
+    fixture = TestBed.createComponent(BankingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it(`
+    (U) getPoupanca():
+    should poupanca have 10
+    `, () => {
+      expect (component.getPoupanca).toEqual(10);
+  });
+
+  it (`
+    (U) getCarteira():
+    should carteira have 50
+    `, () =>{
+      expect(component.getCarteira).toEqual(50);
+  });
+  it (`
+    (U) setSacar ():
+    should transfer poupanca to carteira
+    `, () => {
+      component.setSacar('10');
+      fixture.detectChanges();
+
+      expect(component.getPoupanca).toEqual(0);
+      expect(component.getCarteira).toEqual(60);
+  });
+
+  it (`
+    (U) setSacar(): 
+    should transfer poupanca dont have string (isNaN) 
+    or poupanca  < value
+    `, ()=> {
+      expect (component.setSacar('string')).not.toBeTruthy();
+      expect (component.setSacar('100')).not.toBeTruthy();
+      expect (component.getPoupanca).toEqual(10);
+      expect (component.getCarteira).toEqual(50);
+    });
+
+    it (`
+    (U) setDepositar(): 
+    should transfer carteira dont have string (isNaN) 
+    or carteira  < value
+    `, ()=> {
+      expect (component.setDepositar('string')).not.toBeTruthy();
+      expect (component.setDepositar('100')).not.toBeTruthy();
+      expect (component.getPoupanca).toEqual(10);
+      expect (component.getCarteira).toEqual(50);
+    });
+
+  it(`
+    (U) setDepositar ():
+    should transfer carteira to poupanca
+    `, () => {
+      component.setDepositar('50');
+      fixture.detectChanges();
+
+      expect(component.getCarteira).toEqual(0);
+      expect(component.getPoupanca).toEqual(60);
+  });
+
+
+});
